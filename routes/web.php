534f6166d3bdb185e65a97863f4b5618013c20f3
Alamin <aak75049@gmail.com>
2025-09-19 22:28:13 +0600

    Update web.php
    update for home page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 use App\Http\Controllers\CourseController;
 
-Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+Route::get('/', [CourseController::class, 'create'])->name('courses.create');
 Route::post('/courses', [CourseController::class, 'store'])->name('courses.store');
